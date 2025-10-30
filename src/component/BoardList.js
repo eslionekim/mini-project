599225ece7911e918101a37130aadc15dd4e6ddf
admin <133757594+eslionekim@users.noreply.github.com>
2025-10-30 15:02:54 +0900

    검색창

--- src/component/BoardList.js
+++ src/component/BoardList.js
@@ -1,6 +1,7 @@
 import { useCallback, useEffect, useState } from "react";
 import axios from "axios";
 import { useNavigate,Link } from "react-router-dom";
+import debounce from "lodash.debounce";
 
 const BoardList = () => {
   //게시글 변수
@@ -11,23 +12,50 @@ const BoardList = () => {
   const [totalPages, setTotalPages] = useState(0); // 전체 페이지 수
   const pageSize = 5; // 한 페이지에 보여줄 글 수
 
+  //검색어
+  const [search,setSearch]=useState(""); //검색어 상태
+
   //페이지 이동
   const navigate =useNavigate(); //이거 추가
-  
-  //한 페이지에 보여줄 10개의 글
-  const fetchPosts = useCallback( async (page) => { //async: 서버에서 게시글 데이터 가져오기
+
+  // 서버에서 게시글 가져오기
+  const fetchPosts = useCallback(async (page, keyword = "") => { //pageSize는 이미 있어서
     try {
-      const res = await axios.get(`http://localhost:8080/api/posts?page=${page}&size=${pageSize}`);//api호출
-      setPosts(res.data.content); //객체의 content 가져오기
-      setTotalPages(res.data.totalPages); //객체의 totalPages 가져오기
+      const res = await axios.get(`http://localhost:8080/api/posts?page=${page}&size=${pageSize}${keyword ? `&search=${keyword}` : ""}`);
+      //${keyword ? `&search=${keyword}` : ""} => keyword있으면 search에 넣고 없으면 아얘 공백 
+      setPosts(res.data.content); //게시글 결과를 post에 저장
+      setTotalPages(res.data.totalPages); //전체 페이지수도 가져오기
     } catch (err) {
       console.error(err);
     }
-  },[pageSize]);
+  }, []);
+  // 검색 적용
+  const debouncedFetchPosts = useCallback(
+    debounce((value) => {
+      fetchPosts(0, value); //위 함수(게시글 전체)를 0페이지부터 가져와
+      setPage(0); //const [page, setPage] = useState(0); 현재 페이지 현재 페이지 수를 0으로
+    }, 500), //500ms 지연, 타이핑치는 과정에 있는 텍스트는 무시해야 효율적
+    [fetchPosts] //위 함수 호출시마다
+  );
+  // 검색어 입력시 호출
+  const handleSearchChange = (e) => {
+    const value = e.target.value; //인풋 값 가져와
+    setSearch(value); //const [search,setSearch]=useState(""); search업데이트
+    if(value){ //검색어가 있을때만 
+      debouncedFetchPosts(value); //검색 적용
+    } else {
+        debouncedFetchPosts.cancel(); // 남은 debounce 취소
+        fetchPosts(0);                // 전체 게시글 조회
+        setPage(0);                   // 페이지 초기화
+    }
+  };
 
-  useEffect(() => { //페이지 변경마다 게시글 10개 함수 호출
-    fetchPosts(page);
-  }, [page]);
+  //검색안할때 전체 게시글 가져오기 (페이지나 검색어 바뀔 때 실행)
+  useEffect(() => { 
+    if(search===""){ //검색 중이 아니라면!! 얘를 위한거임
+      fetchPosts(page,search); //전체 게시글 가져오기
+    }
+  }, [page,fetchPosts, search]);
 
   const handlePrev = useCallback(() => { //이전 버튼 함수
     if (page > 0) setPage(page - 1); //page=1부터 일때만
@@ -40,6 +68,13 @@ const BoardList = () => {
   return (
     <div>
       <button onClick={()=>navigate("/board/regist")}>글 작성</button>
+      {/* 검색 input */}
+      <input
+        type="text"
+        placeholder="검색어 입력"
+        value={search}
+        onChange={handleSearchChange}
+      />
       {/* onClick: 클릭 시 실행할 함수 등록, ()=>화살표 함수, navigate(""):페이지 이동함수 */}
       {posts.map(post => (
         <div key={post.id}>
